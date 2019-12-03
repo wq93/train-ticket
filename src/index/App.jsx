f2375286c19073c7a5a9c76306668e80fa4a89e6
wangqi <wangqi@chicv.com>
2019-12-04 07:02:34 +0800

    8-3 add Journey.jsx

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -8,17 +9,42 @@ import HighSpeed from './HighSpeed.jsx';
 import Journey from './Journey.jsx';
 import Submit from './Submit.jsx';
 
-function App() {
+import {
+  exchangeFromTo,
+  showCitySelector
+} from './actions.js';
+
+function App(props) {
+  // 从store中取出from, to
+  const {
+    from,
+    to,
+    dispatch
+  } = props;
 
   const onBack = useCallback(() => {
     // 后退
     window.history.back();
   }, []);
 
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector,
+      },
+      dispatch
+    );
+  }, []);
+
   return (
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack} />
+        <Journey
+          from={ from }
+          to={ to }
+          { ...cbs }></Journey>
       </div>
     </div>
   );
